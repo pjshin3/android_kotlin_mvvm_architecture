d33522195d396a7eb7f0aa705a9a64d75ec1d98d
Philip <philip@swit.io>
2021-03-30 15:47:05 +0900

    espresso simply ui testing code add

--- app/src/androidTest/java/com/example/mymvvm/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/mymvvm/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.mymvvm
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,15 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+
+    @Rule
+    @JvmField
+    val activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.mymvvm", appContext.packageName)
+        onView(withId(R.id.textview)).check(matches(withText("hello new world")))
     }
 }
